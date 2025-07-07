d2b20f902f565bb4f1589ef6f0b70d5abbaafdc9
Siroya Het <166714683+HetSiroya@users.noreply.github.com>
2025-07-07 10:24:00 +0530

    feat: Implement authentication functions including login and change password

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,9 +3,11 @@ import express, { Request, Response } from "express";
 import { checkRequiredFields } from "../helpers/commonValidator";
 import generateToken from "../helpers/token";
 import userModel from "../models/usersModel";
-import { hashPassword } from "../helpers/hased";
+import { comparePassword, hashPassword } from "../helpers/hased";
+import { CustomRequest } from "../middlewares/token-decode";
 
 export const authController = {
+  // Sign Up function
   signUp: async (req: Request, res: Response) => {
     try {
       const { name, email, password, confirmPassword, mobileNumber } = req.body;
@@ -90,14 +92,148 @@ export const authController = {
       });
     }
   },
-  login: (req: Request, res: Response) => {
-    res.status(200).json({
-      message: "Login successful",
-      user: {
-        id: 1,
-        name: "John Doe",
-        email: "",
-      },
-    });
+
+  // Login function
+
+  login: async (req: Request, res: Response) => {
+    try {
+      const { input, password } = req.body;
+      // Check if input is email or mobile number
+      const isEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input);
+      const query = isEmail ? { email: input } : { mobileNumber: input };
+      const user = await userModel.findOne(query);
+      if (!user) {
+        return res.status(400).json({
+          status: false,
+          message: "Invalid credentials",
+          data: "",
+        });
+      }
+      // Compare password
+      const isMatch = await comparePassword(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({
+          status: false,
+          message: "Invalid Password",
+          data: "",
+        });
+      }
+      const tokenUser = {
+        _id: user._id,
+        role: user.role,
+      };
+      const token = generateToken(tokenUser);
+      res.status(200).json({
+        status: true,
+        message: "Login successful",
+        data: user,
+        token: token,
+      });
+    } catch (error: any) {
+      console.log("Error during login:", error.message);
+      res.status(500).json({
+        status: false,
+        message: "Something went wrong",
+        data: "",
+      });
+    }
+  },
+
+  // Change Password function
+  changePassword: async (req: CustomRequest, res: Response) => {
+    try {
+      const userId = req.user._id; // Assuming user ID is stored in req.user
+      const user = await userModel.findById(userId);
+      if (!user) {
+        return res.status(404).json({
+          status: false,
+          message: "User not found",
+          data: "",
+        });
+      }
+      // Validate old password
+      const isMatch = await comparePassword(
+        req.body.oldPassword,
+        user.password
+      );
+      if (!isMatch) {
+        return res.status(400).json({
+          status: false,
+          message: "Old password is incorrect",
+          data: "",
+        });
+      }
+      const { oldPassword, newPassword, confirmNewPassword } = req.body;
+    } catch (error: any) {
+      console.log("Error during login:", error.message);
+      res.status(500).json({
+        status: false,
+        message: "Something went wrong",
+        data: "",
+      });
+    }
+  },
+
+  updateProfile: async (req: CustomRequest, res: Response) => {
+    try {
+      const userId = req.user._id;
+      const user = await userModel.findById(userId);
+      if (!user) {
+        return res.status(404).json({
+          status: false,
+          message: "User not found",
+          data: "",
+        });
+      }
+      const { name, email, mobileNumber } = req.body;
+      // Validate email format
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(email)) {
+        return res.status(400).json({
+          status: false,
+          message: "Invalid email format",
+          data: "",
+        });
+      }
+      // Validate mobile number format
+      const mobileNumberRegex = /^\d{10}$/;
+      if (!mobileNumberRegex.test(mobileNumber)) {
+        return res.status(400).json({
+          status: false,
+          message: "Mobile number must be 10 digits",
+          data: "",
+        });
+      }
+
+      // check if email or mobile number already exists
+      const existUser = await userModel.findOne({
+        $or: [{ email }, { mobileNumber }],
+        _id: { $ne: userId }, // Exclude current user
+      });
+      if (existUser) {
+        return res.status(400).json({
+          status: false,
+          message: "Email or mobile number already exists",
+          data: "",
+        });
+      }
+      // Update user profile
+      user.name = name;
+      user.email = email;
+      user.mobileNumber = mobileNumber;
+      await user.save();
+      res.status(200).json({
+        status: true,
+        message: "Profile updated successfully",
+        data: user,
+      });
+    } catch (error: any) {
+      console.log("Error during login:", error.message);
+      res.status(500).json({
+        status: false,
+        message: "Something went wrong",
+        data: "",
+      });
+    }
   },
 };
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -4,5 +4,7 @@ import { catchAsync } from "../utils/catchAsync";
 const router = express.Router();
 
 router.post("/signup", catchAsync(authController.signUp));
+router.post("/login", catchAsync(authController.login));
+router.patch("/updatePassword", catchAsync(authController.changePassword)); 
 
 export default router;
